e736d7699976e70d7b6ea712b61b172ed3337ab6
T-Lobato <thyagollobato@gmail.com>
2022-08-30 13:22:13 -0300

    Mapeando as Requisições com Request Mapping

--- trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/rest/controller/ClienteController.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/rest/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package org.example.rest.controller;
 
+import org.example.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/api/clientes/hello/{nome}", "/api/hello"},
+            method = RequestMethod.GET,
+            consumes = { "application/json", "application/xml"},
+            produces = { "application/json", "application/xml"}
+    )
+
     @ResponseBody
-    public String helloCliente( @PathVariable("nome") String nomeCliente ) {
+    public Cliente helloCliente(@PathVariable("nome") String nomeCliente ) {
         return String.format("Hello %s ", nomeCliente);
     }
 
