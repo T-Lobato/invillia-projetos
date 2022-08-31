a1416cc2a5d831c95451d471976c2176838dcbd7
T-Lobato <thyagollobato@gmail.com>
2022-08-31 14:14:01 -0300

    Validando a Entidade Produto

--- trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/domain/entity/Produto.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/domain/entity/Produto.java
@@ -3,6 +3,8 @@ package org.example.domain.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @NoArgsConstructor
@@ -17,7 +19,9 @@ public class Produto {
     @Column(name = "id")
     private Integer id;
     @Column(name = "descricao")
+    @NotEmpty(message = "Campo Descrição é obrigatório.")
     private String descricao;
     @Column(name = "preco_unitario")
+    @NotNull(message = "Campo Preço é obrigatório.")
     private BigDecimal preco;
 }
--- trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/rest/controller/ProdutoController.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/rest/controller/ProdutoController.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
 import static org.springframework.http.HttpStatus.*;
 
 import java.util.List;
@@ -32,7 +35,7 @@ public class ProdutoController {
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public Produto save( @RequestBody Produto produto ){
+    public Produto save( @RequestBody @Valid Produto produto ){
         return repository.save(produto);
     }
 
@@ -51,7 +54,7 @@ public class ProdutoController {
     @PutMapping("{id}")
     @ResponseStatus(NO_CONTENT)
     public void update( @PathVariable Integer id,
-                                @RequestBody Produto produto){
+                                @RequestBody @Valid Produto produto){
         repository
                 .findById(id)
                 .map( produtoExistente -> {
