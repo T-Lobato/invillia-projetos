a5b7ee8d9dc0c3629ec2bff5b0b7695dca69e0ea
T-Lobato <thyagollobato@gmail.com>
2022-08-31 14:05:43 -0300

    Utilizando e Testando o Bean Validation

--- trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/rest/controller/ApplicationControllerAdvice.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/rest/controller/ApplicationControllerAdvice.java
@@ -4,10 +4,14 @@ import org.example.exception.RegraNegocioException;
 import org.example.rest.ApiErros;
 import org.example.service.exception.PedidoNaoEncontradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -24,4 +28,16 @@ public class ApplicationControllerAdvice {
     public ApiErros handlePedidoNotFoundException( PedidoNaoEncontradoException ex ){
         return new ApiErros(ex.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErros handleMethodNotValidExeption(MethodArgumentNotValidException ex){
+        List<String> erros = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErros(erros);
+    }
+
+
 }
