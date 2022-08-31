6f6935b3fcf59550c033eb602823b90875b55538
T-Lobato <thyagollobato@gmail.com>
2022-08-31 16:41:15 -0300

    Configurando a autorização de urls

--- trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/config/SecurityConfig.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/config/SecurityConfig.java
@@ -26,7 +26,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    protected void configure( HttpSecurity http ) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                    .antMatchers("/api/clientes/**")
+                        .permitAll()
+                .and()
+                    .formLogin();
+
     }
 }
