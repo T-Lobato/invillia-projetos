fae8b62a82ef249f6549cc9700439cc4df9a7fe8
T-Lobato <thyagollobato@gmail.com>
2022-09-06 15:33:06 -0300

    Query Methods com variantes de Strings

--- trilha-Backend-Insiders/1-spring-boot-com-java/localizacao/src/main/java/com/example/localizacao/domain/repository/CidadeRepository.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/localizacao/src/main/java/com/example/localizacao/domain/repository/CidadeRepository.java
@@ -3,5 +3,25 @@ package com.example.localizacao.domain.repository;
 import com.example.localizacao.domain.entity.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+
+    //Busca pelo nome correto
+    List<Cidade> findByNome(String nome);
+
+    // Busca pelo nome começando por aquele pedaço
+    List<Cidade> findByNomeStartingWith(String nome);
+
+    // Busca pelo nome terminando por aquele pedaço
+    List<Cidade> findByNomeEndingWith(String nome);
+
+    // Busca pelo nome contendo aquele pedaço
+    List<Cidade> findByNomeContaining(String nome);
+
+
+    List<Cidade> findByHabitantes(Long habitantes);
+
+
+
 }
