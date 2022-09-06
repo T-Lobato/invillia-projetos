f7e1757fcf8df5704df58f3fe0d5514d071789dd
T-Lobato <thyagollobato@gmail.com>
2022-09-06 11:11:52 -0300

    Gerando um Arquivo WAR

--- trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/VendasApplication.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/vendas/src/main/java/org/example/VendasApplication.java
@@ -2,11 +2,12 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
