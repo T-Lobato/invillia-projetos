8c1e13a9dd6538505ff6c477a567081a618e0548
T-Lobato <thyagollobato@gmail.com>
2022-09-08 14:54:29 -0300

    POST - @RequestParam

--- trilha-Backend-Insiders/1-spring-boot-com-java/carros/src/main/java/com/example/carros/api/IndexController.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/carros/src/main/java/com/example/carros/api/IndexController.java
@@ -11,21 +11,8 @@ public class IndexController {
         return "Get Spring Boot";
     }
 
-    @GetMapping("/login/{login}/senha/{senha}")
-    public String login(@PathVariable("login") String login, @PathVariable("senha") String senha){
+    @PostMapping("/login")
+    public String login(@RequestParam("login") String login, @RequestParam("senha") String senha){
         return "Login: " + login + ", senha: " + senha;
     }
-
-    @GetMapping("/carros/{id}")
-    public String getCarroById(@PathVariable("id") Long id) {
-        return "Carro by id " + id;
-    }
-
-    @GetMapping("/carros/tipo/{tipo}")
-    public String getCarroByTipo(@PathVariable("tipo") String tipo){
-        return "Lista de Carros " + tipo;
-    }
-
-
-
 }
