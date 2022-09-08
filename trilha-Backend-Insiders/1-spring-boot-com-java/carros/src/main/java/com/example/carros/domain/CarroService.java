5d0cf1e00c721659e135d213aa13c42854b3f2bd
T-Lobato <thyagollobato@gmail.com>
2022-09-08 17:04:25 -0300

    Atualizar o Carro

--- trilha-Backend-Insiders/1-spring-boot-com-java/carros/src/main/java/com/example/carros/domain/CarroService.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/carros/src/main/java/com/example/carros/domain/CarroService.java
@@ -2,7 +2,9 @@ package com.example.carros.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,14 +19,37 @@ public class CarroService {
     public Optional<Carro> getCarroById(Long id) {
         return rep.findById(id);
     }
-    public Iterable<Carro> getCarrosByTipo(String tipo) {
+    public List<Carro> getCarrosByTipo(String tipo) {
         return rep.findByTipo(tipo);
     }
 
-    public Carro save(Carro carro) {
+    public Carro insert(Carro carro){
+        Assert.isNull(carro.getId(), "Não foi possível inserir o registro");
+
         return rep.save(carro);
     }
 
+    public Carro update(Carro carro, Long id){
+        Assert.notNull(id, "Não foi possível atualizar o registro");
+
+        // Busca o carro no banco de dados
+        Optional<Carro> optional = getCarroById(id);
+        if(optional.isPresent()){
+            Carro db = optional.get();
+            // copiar as propriedades
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+            System.out.println("Carro id " + db.getId());
+
+            //Atualiza o carro
+            rep.save(db);
+
+            return db;
+        } else {
+            throw new RuntimeException(("Não foi possível atualizar o registro"));
+        }
+
+    }
 //    public List<Carro> getCarrosFake(){
 //        List<Carro> carros = new ArrayList<>();
 //
