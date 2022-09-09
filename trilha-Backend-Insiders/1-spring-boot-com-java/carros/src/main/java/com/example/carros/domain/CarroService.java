3e36c408ca7f74e767b8b5d307d493a10033f482
T-Lobato <thyagollobato@gmail.com>
2022-09-09 11:51:01 -0300

    DELETE

--- trilha-Backend-Insiders/1-spring-boot-com-java/carros/src/main/java/com/example/carros/domain/CarroService.java
+++ trilha-Backend-Insiders/1-spring-boot-com-java/carros/src/main/java/com/example/carros/domain/CarroService.java
@@ -65,9 +65,11 @@ public class CarroService {
         }
     }
 
-    public void delete(Long id) {
+    public boolean delete(Long id) {
         if(getCarroById(id).isPresent()){
             rep.deleteById(id);
+            return true;
         }
+        return false;
     }
 }
