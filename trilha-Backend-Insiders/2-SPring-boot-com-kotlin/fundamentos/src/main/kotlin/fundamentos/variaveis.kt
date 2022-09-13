f0fd7d4f89dd717a6031f7be4f6aba48fec87de6
T-Lobato <thyagollobato@gmail.com>
2022-09-13 14:06:50 -0300

    Variaveis

---
+++ trilha-Backend-Insiders/2-SPring-boot-com-kotlin/fundamentos/src/main/kotlin/fundamentos/variaveis.kt
@@ -0,0 +1,24 @@
+package fundamentos
+
+fun main() {
+    var nome = "Thyago"
+    val nomeVal = "Thyago"
+
+    nome = "Daniel"
+//    nomeVal = "Daniel"
+
+    val teste: String
+    teste = "Teste"
+
+    var texto = "Texto"
+    texto = 123
+
+}
+
+class variaveis {
+    lateinit var teste: String
+
+    fun iniciaVariaveis(){
+        teste = "Teste"
+    }
+}
