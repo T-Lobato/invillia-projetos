99fa19ea11466cd24d1769202c50c382263106ca
T-Lobato <thyagollobato@gmail.com>
2022-09-14 15:32:24 -0300

    Implementando o Post Customer

--- trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,7 +1,10 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController {
 
     @GetMapping
-    fun helloWord(): CustomerModel{
+    fun getCustomer(): CustomerModel{
         return CustomerModel("1", "Thyago", "email@email.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
     }
 
 
