cca50ed46250ffca15c68b759273b72ea07f6553
T-Lobato <thyagollobato@gmail.com>
2022-09-14 13:44:49 -0300

    Alterando o Path do nosso endpoint

--- trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,15 +1,18 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-
+@RequestMapping("customer")
 class CustomerController {
 
     @GetMapping
     fun helloWord(): String{
-        return "Hello Word"
+        return "Customer 1"
     }
+
+    
 
 }
