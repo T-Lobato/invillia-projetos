6d08fd5c1035b5dc553908659fae7a15337e1a2c
T-Lobato <thyagollobato@gmail.com>
2022-09-14 16:15:49 -0300

    Implementando PUT

--- trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,11 +1,13 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -37,6 +39,15 @@ class CustomerController {
     @GetMapping("/{id}")
     fun getCustomer(@PathVariable id: String): CustomerModel {
         return customers.filter { it.id == id }.first()
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun Update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+         customers.filter { it.id == id }.first().let {
+             it.name = customer.name
+             it.email = customer.email
+         }
     }
 
 }
