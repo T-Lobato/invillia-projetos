4c0c60efa53b0d8dd15b68dad0c43edf0f619338
T-Lobato <thyagollobato@gmail.com>
2022-09-19 11:55:37 -0300

    Criando uma Custom Validation

--- trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
+++ trilha-Backend-Insiders/2-SPring-boot-com-kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
@@ -2,7 +2,7 @@ package com.mercadolivro.controller.request
 
 import java.math.BigDecimal
 
-data class PutBookRequest (
+data class PutBookRequest(
     var name: String?,
     var price: BigDecimal?
-    )
+)
